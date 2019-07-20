d577220e356e5e59b78eb7dc77b8272ae53b0f66
yakir <yakirifrah19@gmail.com>
2019-07-20 11:47:23 +0300

    Fix result search

--- main.py
+++ main.py
@@ -1,48 +1,70 @@
+
 import os
 from bottle import (get, post, redirect, request, route, run, static_file,
                     template, redirect, error)
-import utils
+from utils import get_results, get_version, get_json_from_file, get_data, get_episode, AVAILABE_SHOWS
 import json
 
 
 @post('/search')
 def search_result():
-    sectionTemplate = "./templates/search_result.tpl"
-    query = request.forms.get("q")
-    results = utils.getResults(query)
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={}, results=results, query=query)
+    search_value = request.forms.get("q")
+    results = get_results(search_value)
+    return template(
+        "./pages/index.html",
+        version=get_version(),
+        sectionTemplate='./templates/search_result.tpl',
+        query=search_value,
+        results=results,
+        sectionData={}
+    )
 
 
 @get('/search')
 def search():
     sectionTemplate = "./templates/search.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+    return template(
+        "./pages/index.html",
+        version=get_version(),
+        sectionTemplate=sectionTemplate,
+        sectionData={}
+    )
 
 
 @get('/show/<showId>')
 def show_description(showId):
-    sectionData = json.loads(utils.getJsonFromFile(showId))
+    sectionData = json.loads(get_json_from_file(showId))
     sectionTemplate = "./templates/show.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData=sectionData)
+    return template(
+        "./pages/index.html",
+        version=get_version(),
+        sectionTemplate=sectionTemplate,
+        sectionData=sectionData
+    )
 
 
 @get('/ajax/show/<showId>')
 def show_description(showId):
-    sectionData = json.loads(utils.getJsonFromFile(showId))
+    sectionData = json.loads(get_json_from_file(showId))
     sectionTemplate = "./templates/show.tpl"
     return template(sectionTemplate, result=sectionData)
 
 
 @get('/show/<showId>/episode/<episodeId>')
 def show_episode(showId, episodeId):
-    sectionData = utils.showEpisode(showId, episodeId)
+    sectionData = get_episode(showId, episodeId)
     sectionTemplate = "./templates/episode.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData=sectionData)
+    return template(
+        "./pages/index.html",
+        version=get_version(),
+        sectionTemplate=sectionTemplate,
+        sectionData=sectionData
+    )
 
 
 @get('/ajax/show/<showId>/episode/<episodeId>')
 def show_episode(showId, episodeId):
-    sectionData = utils.showEpisode(showId, episodeId)
+    sectionData = get_episode(showId, episodeId)
     sectionTemplate = "./templates/episode.tpl"
     return template(sectionTemplate, result=sectionData)
 
@@ -50,7 +72,13 @@ def show_episode(showId, episodeId):
 @get('/browse')
 def browse():
     sectionTemplate = "./templates/browse.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData=sorted(utils.showData(), key=lambda show: show["name"]))
+    return template(
+        "./pages/index.html",
+        version=get_version(),
+        sectionTemplate=sectionTemplate,
+        sectionData=sorted(get_data(AVAILABE_SHOWS),
+                           key=lambda show: show["name"])
+    )
 
 
 @get("/js/<filepath:re:.*\.js>")
@@ -71,13 +99,24 @@ def img(filepath):
 @route('/')
 def index():
     sectionTemplate = "./templates/home.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+    return template(
+        "./pages/index.html",
+        version=get_version(),
+        sectionTemplate=sectionTemplate,
+        sectionData={}
+    )
 
 
 @error(404)
 def error404(error):
     sectionTemplate = "./templates/404.tpl"
-    return template("./pages/index.html", version=getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+    return template(
+        "./pages/index.html",
+        version=get_version(),
+        sectionTemplate=sectionTemplate,
+        sectionData={}
+    )
 
 
-run(host='localhost', port=8000, reloader=True, debug=True)
+run(host='localhost', port=os.environ.get(
+    'PORT', 5000), reloader=True, debug=True)
--- utils.py
+++ utils.py
@@ -6,28 +6,43 @@ AVAILABE_SHOWS = ["7", "66", "73", "82", "112", "143",
                   "175", "216", "1371", "1871", "2993", "305"]
 
 
-def showData():
+def get_data(AVAILABE_SHOWS):
     data = []
     for show in AVAILABE_SHOWS:
-        data.append(json.loads(getJsonFromFile(show)))
+        data.append(json.loads(get_json_from_file(show)))
     return data
 
 
-def getVersion():
+def get_version():
     return "0.0.1"
 
 
-def getJsonFromFile(showName):
+def get_json_from_file(showName):
     try:
         return template("{folder}/{filename}.json".format(folder=JSON_FOLDER, filename=showName))
     except:
         return "{}"
 
 
-def showEpisode(showId, episodeId):
-    sectionData = json.loads(getJsonFromFile(showId))
+def get_episode(showId, episodeId):
+    sectionData = json.loads(get_json_from_file(showId))
     for ep in sectionData["_embedded"]["episodes"]:
         if ep["id"] == int(episodeId):
             sectionData = ep
-            break
     return sectionData
+
+
+def get_results(search_value):
+    shows = get_data(AVAILABE_SHOWS)
+    results = []
+    for show in shows:
+        for episode in show["_embedded"]["episodes"]:
+            if search_value.lower() in shows[0]["name"].lower():
+                print(f'res:true')
+            if (search_value.lower() in episode['name'].lower() or search_value.lower() in show['name'].lower()):
+                results.append(
+                    {'showid': show['id'], 'episodeid': episode['id'], 'text': f"{show['name']}: {episode['name']}"})
+            elif episode['summary'] and (search_value.lower() in episode['summary'].lower()):
+                results.append(
+                    {'showid': show['id'], 'episodeid': episode['id'], 'text': f"{show['name']}: {episode['name']}"})
+    return results
